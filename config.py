4665a5e031e51eda5c059908b906e0be2aef00c8
amit2255 <crm.amit@gmail.com>
2020-11-01 16:43:28 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
+customuser = "employee"
+custompass = "employee123"
+customdb = "employee"
+custombucket = "employees3"
+customregion = "us-east-2"
