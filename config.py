29263de9c48cb9fd4e427936dc95cfd1591de17d
amit2255 <crm.amit@gmail.com>
2021-07-02 20:02:15 +0530

    Changed the password

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.coghw13fheqo.us-east-2.rds.amazonaws.com"
 customuser = "admin"
-custompass = "employee123"
+custompass = "employee1234"
 customdb = "employee"
 custombucket = "employees3"
 customregion = "us-east-2"
